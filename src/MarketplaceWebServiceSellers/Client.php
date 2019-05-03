4081f1678911e818dc4bb7dc2adab7009969dc3e
James Allsup <james@allsup.eu>
2019-05-04 00:30:39 +0100

    Update to 2015-06-18

--- src/MarketplaceWebServiceSellers/Client.php
+++ src/MarketplaceWebServiceSellers/Client.php
@@ -1,6 +1,6 @@
 <?php
 /*******************************************************************************
- * Copyright 2009-2015 Amazon Services. All Rights Reserved.
+ * Copyright 2009-2018 Amazon Services. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  *
  * You may not use this file except in compliance with the License. 
@@ -13,8 +13,8 @@
  * @category Amazon
  * @package  Marketplace Web Service Sellers
  * @version  2011-07-01
- * Library Version: 2015-03-12
- * Generated: Wed Mar 11 20:33:07 GMT 2015
+ * Library Version: 2015-06-18
+ * Generated: Wed Sep 12 08:12:03 PDT 2018
  */
 
 /**
@@ -30,7 +30,7 @@ class MarketplaceWebServiceSellers_Client implements MarketplaceWebServiceSeller
 {
 
     const SERVICE_VERSION = '2011-07-01';
-    const MWS_CLIENT_VERSION = '2015-03-12';
+    const MWS_CLIENT_VERSION = '2015-06-18';
 
     /** @var string */
     private  $_awsAccessKeyId = null;
